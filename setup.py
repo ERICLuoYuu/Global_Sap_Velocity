111a7e1b0e3659e871f8e61e1b84ab71f691b23d
Yu Luo <yu.luo@uni-muenster.de>
2024-11-08 17:56:11 +0100

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sap_velocity',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Global Sap Velocity Analysis',
+    author='Your Name',
+    license='MIT',
+)

96b45b8d8be617c1187971da1b4e40bc62d47c46
Yu Luo <yu.luo@uni-muenster.de>
2025-01-08 14:13:18 +0100

    add data files

--- data/raw/grided/plant trait/iNaturalist_traits-v1.0/sojwolf-iNaturalist_traits-0f0cfec/docs/_sources/Header Preprocessing.md
+++ data/raw/grided/plant trait/iNaturalist_traits-v1.0/sojwolf-iNaturalist_traits-0f0cfec/docs/_sources/Header Preprocessing.md
@@ -1,1 +1,3 @@
-# Preprocessing
+version https://git-lfs.github.com/spec/v1
+oid sha256:f1e78d77284b9a66c07e9a017d407a15dd317bb78929053337cd974f75bb3050
+size 16
--- data/raw/grided/plant trait/iNaturalist_traits-v1.0/sojwolf-iNaturalist_traits-0f0cfec/docs/_static/documentation_options.js
+++ data/raw/grided/plant trait/iNaturalist_traits-v1.0/sojwolf-iNaturalist_traits-0f0cfec/docs/_static/documentation_options.js
@@ -1,14 +1,3 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    BUILDER: 'html',
-    FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '',
-    NAVIGATION_WITH_KEYS: true,
-    SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:41531b8742f9cf59137b91123fafc7f7694f5b953fb729295ad9e5434c338346
+size 412
